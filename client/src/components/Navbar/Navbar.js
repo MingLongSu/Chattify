045cf7373ca54bb7d0cc27b0403967828c58c4c1
MingLongSu <bananafrenzy31@gmail.com>
2022-02-23 23:58:24 -0500

    Navigational nav bar implemented

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,31 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import ChattifyLogo from '../../assets/chattifyLogoSVGComponent';
 import './navbar.css';
 
 const Navbar = () => {
-  return (
-    <div className='Navbar__nav-predecessor'>
-        <div className='nav-predecessor__nav-bar'>
-            <div className='nav-bar__chattify-logo-and-title-container'>
-                <ChattifyLogo width={ 130 } height={ 80 }/>
-                <span className='chattify-logo-and-title-container__title'>
-                    Chattify
-                </span>
+    const navigate = useNavigate();
+    const [isOnAbout, setIsOnAbout] = useState(false);
+    const [isOnChats, setIsOnChats] = useState(false);
+    const [isOnLogin, setIsOnLogin] = useState(false);
+
+    // navigates to a particular page based on the button's described location
+    const navigatePages = (e) => { 
+        let pageName = e.target.innerHTML.toLowerCase();
+        pageName = (pageName === 'login' ? '' : pageName);
+        navigate(`/${ pageName }`);
+
+        if (pageName === 'about') { 
+            setIsOnAbout(true);
+            setIsOnChats(false);
+            setIsOnLogin(false);
+        }
+        else if (pageName === 'chats') { 
+            setIsOnAbout(false);
+            setIsOnChats(true);
+            setIsOnLogin(false);
+        }
+        else if (pageName === '') { 
+            setIsOnAbout(false);
+            setIsOnChats(false);
+            setIsOnLogin(true);
+        }
+    }
+
+    return (
+        <div className='Navbar__nav-predecessor'>
+            <div className='nav-predecessor__nav-bar'>
+                <div className='nav-bar__chattify-logo-and-title-container'>
+                    <ChattifyLogo width={ 130 } height={ 80 }/>
+                    <span className='chattify-logo-and-title-container__title'>
+                        Chattify
+                    </span>
+                </div>
+                <nav className='nav-bar__nav'>
+                    <button onClick={ navigatePages } className={ 'nav__button' + (isOnAbout ? ' active' : '') }>
+                        About
+                    </button>
+                    <button onClick={ navigatePages } className={ 'nav__button' + (isOnChats ? ' active' : '') }>
+                        Chats
+                    </button>
+                    <button onClick={ navigatePages } className={ 'nav__button' + (isOnLogin ? ' active' : '') }>
+                        Login
+                    </button>   
+                </nav>
             </div>
-            <nav className='nav-bar__nav'>
-                <button className='nav__button'>
-                    About
-                </button>
-                <button className='nav__button'>
-                    Chats
-                </button>
-                <button className='nav__button'>
-                    Login
-                </button>   
-            </nav>
         </div>
-    </div>
   )
 }
 
