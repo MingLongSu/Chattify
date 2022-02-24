60d8e09f500ee21f2147f290c230428a795a648d
MingLongSu <bananafrenzy31@gmail.com>
2022-02-23 20:46:26 -0500

    Implemented dummy code to work the backend

--- server/server.js
+++ server/server.js
@@ -1,3 +1,6 @@
+// for testing using dummy code
+const { chats } = require("./dummy_data/dummy_data")
+
 const express = require("express");
 const app = express();
 
@@ -5,4 +8,14 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => { 
     console.log(`The server is now listening on PORT ${ PORT }`);
-})
+});
+
+// template function for getting data on desired chats
+app.get("/chat/:id", (req, res) => { 
+    const requestedChatId = req.params.id;
+    const desiredChat = chats.find(chat => 
+        chat.chatId === requestedChatId
+    );
+
+    res.send(desiredChat);
+});
